e26ddedadb3728d1159d95cbf5e2e53a55902bc7
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2015-11-24 12:58:05 -0300

    remove migration script

--- post_install.sh
+++ post_install.sh
@@ -2,7 +2,7 @@
 ./node_modules/.bin/bower install
 
 if [ "$NODEJS_ENV" = "production" ]; then
-  ./node_modules/.bin/sequelize --env production db:migrate
+  # ./node_modules/.bin/sequelize --env production db:migrate
 else
   echo "Setting .env"
   node setup.js
